fbdb894a4fd038895add98436e347740117e3e1f
Victor Velazquez <vvelazquez@bapcorporativo.com>
2023-09-16 22:43:18 -0600

    mansory

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,26 +1,32 @@
 <template>
-  <v-row class="pa-2 ma-0">
-    <v-col
-      cols="6"
-      md="4"
-      lg="3"
-      class="pa-1"
-      v-for="t in TASK_COMPUTED"
-      :key="t._id"
-    >
-      <card
-        :id="t._id"
-        :title="t.title"
-        :is_completed="t.is_completed"
-        :details="t.details"
-        :due_date="t.due_date"
-      />
-      <!--Rendering other component for no tasks-->
-      <div v-if="TASK_COMPUTED.length === 0">
-        <h4 class="text-center">No tasks !</h4>
-      </div>
-    </v-col>
-  </v-row>
+  <div>
+    <v-row class="pa-2 ma-0">
+      <v-col cols="6" md="4" class="pa-1">
+        <div v-for="(t, index) in TASK_COMPUTED" :key="t._id" class="pb-1">
+          <card
+            v-if="index % 2 == 0"
+            :id="t._id"
+            :title="t.title"
+            :is_completed="t.is_completed"
+            :details="t.details"
+            :due_date="t.due_date"
+          />
+        </div>
+      </v-col>
+      <v-col cols="6" md="4" class="pa-1">
+        <div v-for="(t, index) in TASK_COMPUTED" :key="t._id" class="pb-1">
+          <card
+            v-if="index % 2 != 0"
+            :id="t._id"
+            :title="t.title"
+            :is_completed="t.is_completed"
+            :details="t.details"
+            :due_date="t.due_date"
+          />
+        </div>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
